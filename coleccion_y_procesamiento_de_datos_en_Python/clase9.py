358bda108883ca15a061e997be4cf9f8508eb98f
Eduardo <eduar2011.er36@gmail.com>
2024-10-22 20:59:14 -0600

    Comienzo de segundo módulo

---
+++ coleccion_y_procesamiento_de_datos_en_Python/clase9.py
@@ -0,0 +1,5 @@
+# Listas en Python
+## Ejemplo con una lista de quehaceres
+
+to_do = ['Lavar los platos', 'Barrer el cuarto', 'Organizar escritorio', 'Hacer la comida', 'Tirar la basura'];
+print(to_do);
