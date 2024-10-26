78b621fa298f4a2c08b0157ba79cbe7b36f54880
Eduardo <eduar2011.er36@gmail.com>
2024-10-25 18:07:56 -0600

    Agregando archivos de las clases del módulo 3. Empezando módulo 4 y agregando archivo.

--- 3.control_de_flujo_en_python/Ejercicios_practica_clase15.py
+++ 3.control_de_flujo_en_python/Ejercicios_practica_clase15.py
@@ -19,3 +19,37 @@ for i in x:
 
 for i in range(1, 101):
     print(i, end=" ");
+
+#**Imprimir el cuadrado de los números**  
+    #Utiliza un bucle `for` para imprimir el cuadrado de los números del 1 al 10.
+
+for i in range(1, 11):
+    print(i ** 2, end= " ");
+
+
+#**Sumar números pares en un rango**  
+    #Escribe un bucle `for` que sume todos los números pares entre 1 y 50. Al final, imprime la suma total.
+
+for i in range(1, 50):
+    suma = i * 2;
+    if i %2 == 0:
+        print(suma);
+
+#**Imprimir elementos de una lista en mayúsculas**  
+    #Dada una lista de palabras `["python", "java", "c++", "javascript"]`, escribe un bucle `for` que imprima cada palabra en mayúsculas.
+
+mayusculas = ["python", "java", "c++", "javascript"]
+for i in mayusculas:
+    print(i.upper())
+
+# **Imprimir números mayores a 5**  
+    #Dada la lista `[3, 5, 7, 9, 2, 8, 1]`, escribe un bucle `for` con un `if` que imprima solo los números mayores a 5.
+
+numeros = [3, 5, 7, 9, 2, 8, 1];
+for i in numeros:
+    if i > 5:
+        print(i, end=" ");
+
+
+## while exercises
+
