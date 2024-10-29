98c9d1d485286fe69104fd68918adfb6b59eb21a
Eduardo <eduar2011.er36@gmail.com>
2024-10-28 22:05:31 -0600

    Agregando archivos del módulo 4.

--- 4.Funciones_y_manejo_de_excepciones_en_Python/clase18.py
+++ 4.Funciones_y_manejo_de_excepciones_en_Python/clase18.py
@@ -2,3 +2,15 @@
 
 add = lambda a, b: a + b
 print(add(35, 5))
+
+# Ejemplo de uso de lambda con función map()
+
+numeros = [1, 2, 3, 4]
+resultado = list(map(lambda x: x ** 2, numeros))
+print(resultado)
+
+## Obteniendo números pares del 0 al 10
+
+numbers = range(11)
+pares = list(filter(lambda x: x%2 == 0, numbers))
+print(pares)
